de0fe967c6b31da6d64c32e08df731667859ed8f
RochaOscarCol <oscar.rocha.co@gmail.com>
2024-05-24 09:58:43 -0500

    Create initial project with delete some code lines

---
+++ README.old.md
@@ -0,0 +1,2 @@
+# project_gallery_v02
+Is the second try to gallery project training
---
+++ src/App.js
@@ -0,0 +1,31 @@
+//  import logo from './logo.svg';      //Eliminado en el inicio del proyecto
+import './App.css';
+
+function App() {
+  return (
+    <div className="">
+      <h1>
+        Hola Querido Proyecto
+      </h1>
+    </div>      
+      
+//      <header className="App-header">
+//        <img src={logo} className="App-logo" alt="logo" />
+//        <p>
+//          Edit <code>src/App.js</code> and save to reload.
+//        </p>
+//        <a
+//          className="App-link"
+//          href="https://reactjs.org"
+//          target="_blank"
+//          rel="noopener noreferrer"
+//        >
+//          Learn React
+//        </a>
+//      </header>
+      
+    
+  );
+}
+
+export default App;
